554562dc2e2c7dc9f0f710563311a23a1eec0fca
JerryCandy <jeff070135@gmail.com>
2023-10-24 11:44:00 +0800

    week07

---
+++ week07/week07-5.py
@@ -0,0 +1,6 @@
+#字典、字串
+s= "abcdabcd"#字串
+d={}#dict字典
+for c in s:
+  d[c]=1
+  print('現在讀到的字母', c,"字典變成", d)
---
+++ week07/week07-6.py
@@ -0,0 +1,9 @@
+#字典、字串
+s= "abcdabcd"#字串
+d={}#dict字典
+for c in s:
+  if c in d:
+    d[c]=d[c]+1
+  else:
+    d[c]=1
+  print('現在讀到的字母', c,"字典變成", d)
